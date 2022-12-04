88bd7d2b2b8eaf05ec750116bf945d5ecaead070
elshiraphine <erviana.205025@mhs.its.ac.id>
2022-12-04 21:28:02 +0700

    feat: add Login Page

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import {MainView} from "../components/Main";
+import {LoginForm} from "../components/Login/Form";
+
+export default function Login () {
+    return (
+        <>
+            <MainView>
+                <LoginForm />
+            </MainView>
+        </>
+    )
+}
