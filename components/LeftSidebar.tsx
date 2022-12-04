6b826611080cadbf1bd33e7fd23cdbce39011174
elshiraphine <erviana.205025@mhs.its.ac.id>
2022-12-04 14:30:18 +0700

    feat: add LeftSidebar for Dashboard

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,27 @@
+import { Sidebar } from "flowbite-react";
+
+export const LeftSidebar = () => {
+  return (
+    <>
+      <div className="fixed left-0 top-0 bottom-0 w-72 py-2 border-r-2 border-gray-100">
+        <Sidebar>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="/dashboard">Dashboard</Sidebar.Item>
+              <Sidebar.Collapse label="Marketplace" className="text-sm">
+                <Sidebar.Item href="#">Marketplace Anda</Sidebar.Item>
+                <Sidebar.Item href="#">Tambah Marketplace</Sidebar.Item>
+              </Sidebar.Collapse>
+              <Sidebar.Collapse label="Stok Produk">
+                <Sidebar.Item href="#">Lihat Stok</Sidebar.Item>
+                <Sidebar.Item href="#">Tambah Produk</Sidebar.Item>
+              </Sidebar.Collapse>
+              <Sidebar.Item href="#">Laporan</Sidebar.Item>
+              <Sidebar.Item href="#">Statistik</Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      </div>
+    </>
+  );
+};
