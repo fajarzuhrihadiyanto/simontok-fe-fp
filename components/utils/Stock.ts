74edf15a225d58d8c4dfded5bbda1890eaf02795
elshiraphine <erviana.205025@mhs.its.ac.id>
2022-12-09 01:05:43 +0700

    feat: add stock page

---
+++ components/utils/Stock.ts
@@ -0,0 +1,81 @@
+export type stock = {
+    product_name: string;
+    stock: number;
+};
+
+export const stocks: stock[] = [
+    {
+        product_name: "Maybelline Hypercurl Mascara",
+        stock: 120
+    },
+    {
+        product_name: "Product 2",
+        stock: 120
+    },
+    {
+        product_name: "Product 3",
+        stock: 120
+    },
+    {
+        product_name: "Product 4",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    },
+    {
+        product_name: "Maybelline Hypercurl Mascara",
+        stock: 120
+    },
+    {
+        product_name: "Product 2",
+        stock: 120
+    },
+    {
+        product_name: "Product 3",
+        stock: 120
+    },
+    {
+        product_name: "Product 4",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    },
+    {
+        product_name: "Maybelline Hypercurl Mascara",
+        stock: 120
+    },
+    {
+        product_name: "Product 2",
+        stock: 120
+    },
+    {
+        product_name: "Product 3",
+        stock: 120
+    },
+    {
+        product_name: "Product 4",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    },
+    {
+        product_name: "Product 5",
+        stock: 120
+    }
+
+]
+
