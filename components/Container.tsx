df0d59a2bbba2c9bd4e0d70f9aa7151eeb83224c
elshiraphine <erviana.205025@mhs.its.ac.id>
2022-12-12 22:22:09 +0700

    feat: add container component to any dashboard page

---
+++ components/Container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+export const PageContainer: React.FC<ContainerProps> = ({ children }) => {
+    return (
+        <>
+            <div className="bg-white rounded-lg p-8 mt-6">
+                { children }
+            </div>
+        </>
+    )
+}
