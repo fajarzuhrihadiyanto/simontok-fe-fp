38e723db8930445cc1b0dea292a979be4233ce5f
elshiraphine <erviana.205025@mhs.its.ac.id>
2022-12-14 13:41:49 +0700

    style: disable outline in form input

--- components/Register/Form.tsx
+++ components/Register/Form.tsx
@@ -13,14 +13,14 @@ export const RegisterForm = () => {
                 </div>
                 <form className="flex-col space-y-4">
                 <div className="flex flex-col space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
-                    <input type="text" className="w-full px-4 py-2 rounded-md sm:w-1/2 bg-gray-50" placeholder="Nama Depan" required/>
-                    <input type="text" className="w-full px-4 py-2 rounded-md sm:w-1/2 bg-gray-50" placeholder="Nama Belakang" required/>
+                    <input type="text" className="w-full p-3 rounded-lg sm:w-1/2 bg-light border border-white" placeholder="Nama Depan" required/>
+                    <input type="text" className="w-full p-3 rounded-lg sm:w-1/2 bg-light border border-white" placeholder="Nama Belakang" required/>
                 </div>
-                    <input type="email" className="w-full px-4 py-2 rounded-md bg-gray-50" placeholder="Email" required/>
-                    <input type="password" className="w-full px-4 py-2 rounded-md bg-gray-50" placeholder="Kata Sandi" required/>
+                    <input type="email" className="w-full p-3 rounded-lg border border-white bg-light" placeholder="Email" required/>
+                    <input type="password" className="w-full p-3 rounded-lg border border-white bg-light" placeholder="Kata Sandi" required/>
                     <p className="mb-6 text-xs text-justify text-gray-400">Dengan mengklik Daftar, Anda menyetujui Ketentuan, Kebijakan Data dan Kebijakan
                         Cookie kami. Anda akan menerima email dari Simontok dan dapat menolaknya kapan saja.</p>
-                    <button className="w-full px-4 py-2 rounded-md bg-primary hover:bg-forhoverprimary">
+                    <button className="w-full p-3 rounded-md bg-primary hover:bg-forhoverprimary">
                         <p className="text-center text-white">Daftar</p>
                     </button>
                 </form>
